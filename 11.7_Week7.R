614ec44ae49fc767a1499ebad3600faaf9930c8d
Stanleyismeeeeee <s411273027@gm.ntpu.edu.tw>
2023-11-07 22:28:11 +0800

    11/7 (Week7)
    還有一支影片

---
+++ 11.7_Week7.R
@@ -0,0 +1,50 @@
+
+flights <- readRDS("data/flights.rds")
+str(flights)
+
+flightsData <- flights$data[[1]]$data_frame
+# Or
+flightsData <- flights[["data"]][[1]][["data_frame"]]
+
+# look at your data 
+install.packages("dplyr")
+dplyr::glimpse(flights$data[[1]]$data_frame)
+
+# ERROR package dplyr not found 
+
+head(flightsData)
+
+class(flightsData$AirlineID)
+
+"1" +3
+"2023-01-14" + 1
+
+householdIncomes <- c(
+  "middle income", "high income", "middle income", "low income",
+  "middle income", "high income", "high income", "low income",
+  "high income", "middle income", "high income", "middle income"
+)
+
+class(householdIncomes)
+
+householdIncomes[[1]] < "high incomes"
+
+flightsData$AirlineID <- factor(
+  flightsData$AirlineID
+)
+
+class(flightsData$AirlineID)
+head(flightsData$AirlineID)
+
+
+fct_householdIncome <- factor(householdIncomes)
+ord_fct_householdIncome <- ordered(householdIncomes, levels = c("low income", "middle income", "high income"))
+
+fct_householdIncome[[1]] < "high income"
+levels(fct_householdIncome)
+
+
+ ord_fct_householdIncome[[1]] < "high income"
+levels(ord_fct_householdIncome)
+
+"middle income" < "high income"
